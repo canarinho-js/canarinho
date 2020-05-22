419ac0b7ec0d226e44dc2c66dbb1237a9103eb6a
hsangalli <habyte@gmail.com>
2020-05-21 23:58:40 -0300

    Transpilação reversa II

--- cli.js
+++ cli.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
 var transpilar = require('./codigo_fonte/transpilador_arquivos').transpilar;
+var transpilar_inversamente = require('./codigo_fonte/transpilador_arquivos').transpilar_inversamente;
 var transpilar_recursivamente = require('./codigo_fonte/transpilador_arquivos').transpilar_recursivamente;
+var transpilar_inversamente_recursivamente = require('./codigo_fonte/transpilador_arquivos').transpilar_inversamente_recursivamente;
 
 var argumentos = process.argv.slice(2);
 var caminho_relativo = argumentos[argumentos.length - 1];
 
+var traducao_deve_ser_inversa = argumentos.indexOf("-i") >= 0;
 var traducao_deve_ser_recursiva = argumentos.indexOf("-r") >= 0;
 
-if (traducao_deve_ser_recursiva) {
-  transpilar_recursivamente(caminho_relativo);
+if (traducao_deve_ser_inversa) {
+  if (traducao_deve_ser_recursiva) {
+    transpilar_inversamente_recursivamente(caminho_relativo)
+  } else {
+    transpilar_inversamente(caminho_relativo)
+  }
 } else {
-  transpilar(caminho_relativo);
+  if (traducao_deve_ser_recursiva) {
+    transpilar_recursivamente(caminho_relativo);
+  } else {
+    transpilar(caminho_relativo);
+  }
 }
+
--- codigo_fonte/transpilador_arquivos.js
+++ codigo_fonte/transpilador_arquivos.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
 var traduzir_para_js = require('./transpilador').traduzir_para_js;
+var traduzir_para_canarinho = require('./transpilador').traduzir_para_canarinho;
 
 function transpilar(arquivo_canarinho) {
   var conteudo = fs.readFileSync(arquivo_canarinho, "utf8");
   var conteudo_traduzido = traduzir_para_js(conteudo);
 
   var arquivo_destino = arquivo_canarinho + ".js";
+
+  fs.writeFileSync(arquivo_destino, conteudo_traduzido);
+}
+
+function transpilar_inversamente(arquivo_javascript) {
+  var conteudo = fs.readFileSync(arquivo_javascript, "utf8");
+  var conteudo_traduzido = traduzir_para_canarinho(conteudo);
+
+  var arquivo_destino = arquivo_javascript + ".cnr";
 
   fs.writeFileSync(arquivo_destino, conteudo_traduzido);
 }
@@ -20,7 +30,19 @@ function transpilar_recursivamente(pasta_de_arquivos_canarinho) {
   })
 }
 
+function transpilar_inversamente_recursivamente(pasta_de_arquivos_javascript) {
+  var pasta = fs.readdirSync(pasta_de_arquivos_javascript);
+
+  pasta.forEach(function (arquivo) {
+    if (arquivo.indexOf(".js") > -1) {
+      transpilar_inversamente(pasta_de_arquivos_javascript + '/' + arquivo);
+    }
+  })
+}
+
 module.exports = {
   transpilar,
+  transpilar_inversamente,
   transpilar_recursivamente,
+  transpilar_inversamente_recursivamente
 }
